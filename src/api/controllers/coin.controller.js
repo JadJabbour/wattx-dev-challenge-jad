28bf93e9ec569cb122dd4888a90176a05ff1b724
Jad Jabbour <jad.jabbour@outlook.com>
2018-12-21 23:04:22 +0200

    initial commit

--- src/api/controllers/coin.controller.js
+++ src/api/controllers/coin.controller.js
@@ -3,8 +3,9 @@ const Coin = require('../models/coin.model');
 exports.list = async (req, res, next) => {
 	try{
 		var limit = 200;
+		console.log(req.query);
 
-		if(req.query.hasOwnProperty('limit') && Number.isInteger(req.query.limit) && req.query.limit < 200){
+		if(req.query.hasOwnProperty('limit') && parseInt(req.query.limit) < 200){
 			limit = parseInt(req.query.limit);
 		}
 
